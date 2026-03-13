10ba525d3d1fb4e1ed57734ac02b9be60d4fe472
Rafael <rafaelmerlotto@gmail.com>
2026-03-13 14:57:19 +0100

    Add pwa in the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'MicroSaaS',
+        short_name: 'MicroSaaS',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: './public/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './public/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
