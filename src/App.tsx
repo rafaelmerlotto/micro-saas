facb75dbc1898dc1c43b3b34b12b7bf38b77a698
Rafael <rafaelmerlotto@gmail.com>
2026-03-13 06:32:25 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from '../src/assets/images/logo.png'
+
+
+function App() {
+
+  return (
+    <div className=" h-screen text-white flex items-center justify-center">
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default App
