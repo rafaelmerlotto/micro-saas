399f25422f08c754290842b70d716e5fc0e51a80
Rafael <rafaelmerlotto@gmail.com>
2026-05-29 23:34:45 +0200

    Fix

--- src/components/GithubLoginButton.tsx
+++ src/components/GithubLoginButton.tsx
@@ -7,7 +7,7 @@ const GithubLoginButton = () => {
 
     return (
         <form
-            action="http://localhost:3000/api/v1/users/auth/github"
+            action="https://micro-saas-si.onrender.com/api/v1/users/auth/github"
             method="POST"
         >
             <button type="submit" className="flex items-center justify-center gap-3 px-5 py-2.5 bg-white border border-gray-300 rounded-lg shadow-sm hover:shadow-md hover:bg-gray-50 active:scale-[0.98] transition-all duration-150 font-medium text-gray-700">
