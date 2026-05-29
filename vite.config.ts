716e4d0b92a4b40c674d49f49c647e0be4259fde
Rafael <rafaelmerlotto@gmail.com>
2026-05-29 23:30:09 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false
+  },
   plugins: [
     react(),
     tailwindcss(),
