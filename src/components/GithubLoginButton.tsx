88e56b334d75a8c0b01ebc95fce4794f95077f01
Rafael <rafaelmerlotto@gmail.com>
2026-06-01 12:46:31 +0200

    Fix

--- src/components/GithubLoginButton.tsx
+++ src/components/GithubLoginButton.tsx
@@ -2,7 +2,7 @@
 const GithubLoginButton = () => {
     const handleGithubLogin = () => {
         const apiUrl: string = import.meta.env.VITE_API_URL;
-        window.location.href = `http://localhost:3000/api/v1/users/auth/github`;
+        window.location.href = `${apiUrl}/api/v1/users/auth/github`;
     };
 
     return (
